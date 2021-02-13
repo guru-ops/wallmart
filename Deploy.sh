dd21680eba4d22c51ea0456f514d55d4bcbdccc5
gurumadala <gurumadala@gmail.com>
2021-02-13 23:11:50 +0530

    Revert "update 3"
    This reverts commit a3c36e5f7ce6bf01ea0e855da0e5d584a7bef9f2.

--- Deploy.sh
+++ Deploy.sh
@@ -2,4 +2,8 @@ last update 5th Feb 21:00: dev
 demo for stash
 last update 5th Fenb 19:15`
 78910
-update on 13th Feb 22:58
+<<<<<<< HEAD
+=======
+
+new line added to test git rebase on 11th feb 22.23
+>>>>>>> f533a2ae73c09f69791c34b60d9bd29d54a22441
