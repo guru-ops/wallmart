450f8f71258580a20e3327de9a38739bab8b0768
gurumadala <gurumadala@gmail.com>
2021-02-13 23:14:21 +0530

    udate 4

--- Deploy.sh
+++ Deploy.sh
@@ -2,8 +2,6 @@ last update 5th Feb 21:00: dev
 demo for stash
 last update 5th Fenb 19:15`
 78910
-<<<<<<< HEAD
-=======
 
 new line added to test git rebase on 11th feb 22.23
->>>>>>> f533a2ae73c09f69791c34b60d9bd29d54a22441
+
